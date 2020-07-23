31234639ed6519844cbd8369e80be2c7491b9d9a
Oisee <oicosamaishtiaq@hotmail.com>
2020-07-23 17:36:55 +0500

    updated database

--- MYSQL/db_videos.sql
+++ MYSQL/db_videos.sql
@@ -6,11 +6,11 @@ MySQL - 10.1.9-MariaDB : Database - db_videos
 
 /*!40101 SET NAMES utf8 */;
 
-/*!40101 SET SQL_MODE=''*/;
+/*!40101 SET SQL_MODE=``*/;
 
 /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE=`NO_AUTO_VALUE_ON_ZERO` */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 CREATE DATABASE /*!32312 IF NOT EXISTS*/`db_videos` /*!40100 DEFAULT CHARACTER SET latin1 */;
 
@@ -23,17 +23,66 @@ DROP TABLE IF EXISTS `videos`;
 CREATE TABLE `videos` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `file_name` varchar(255) NOT NULL,
-  `original_name` varchar(255) NOT NULL,
   `video_name` varchar(255) NOT NULL,
   `slug` varchar(255) NOT NULL,
   `size` varchar(50) NOT NULL,
   `mimetype` varchar(255) NOT NULL,
   `encoding` varchar(255) NOT NULL,
   `created_at` datetime DEFAULT CURRENT_TIMESTAMP,
+  `user_id` int(11) NOT NULL,
+  `file_path` varchar(1000) NOT NULL,
+  `process_status` varchar(100) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=latin1;
 
-/*Data for the table `videos` */
+DROP TABLE IF EXISTS `app_settings`;
+
+CREATE TABLE `app_settings` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `selected_preview` varchar(250) NOT NULL,
+  `bit_rate` varchar(250) NOT NULL,
+  `created_at` datetime DEFAULT CURRENT_TIMESTAMP,
+PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=latin1;
+
+DROP TABLE IF EXISTS `remote_storages`;
+
+CREATE TABLE `remote_storages` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `storage_type` varchar(250) NOT NULL,
+  `server_name` varchar(500) NOT NULL,
+  `user_name` varchar(500) NOT NULL,
+  `password` varchar(500) NOT NULL,
+  `authentication_type` varchar(500) NOT NULL,
+  `port` varchar(250) NOT NULL,
+  `created_at` datetime DEFAULT CURRENT_TIMESTAMP,
+PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=latin1;
+
+DROP TABLE IF EXISTS `upload_logs`;
+
+CREATE TABLE `upload_logs` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `temp_path` varchar(1000) NOT NULL,
+  `order_index` int(11) NOT NULL,
+  `data_type` varchar(250) NOT NULL,
+  `process_status` varchar(250) NOT NULL,
+  `created_at` datetime DEFAULT CURRENT_TIMESTAMP,
+PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=latin1;
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `email` varchar(1000) NOT NULL,
+  `password` int(11) NOT NULL,
+  `is_verified` varchar(250) NOT NULL,
+  `first_name` varchar(250) NOT NULL,
+  `last_name` varchar(250) NOT NULL,
+  `created_at` datetime DEFAULT CURRENT_TIMESTAMP,
+PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=latin1;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
